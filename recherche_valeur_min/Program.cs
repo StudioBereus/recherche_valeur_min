2583787bdf08836949034fbb46459d2bb1a96b3e
osalysdecatarina <72741076+osalysdecatarina@users.noreply.github.com>
2020-12-01 16:46:53 +0100

    modification

--- recherche_valeur_min/Program.cs
+++ recherche_valeur_min/Program.cs
@@ -6,23 +6,22 @@ namespace recherche_valeur_min
     {
         static void Main(string[] args)
         {
-            int taille;
+            
             Console.WriteLine("Programme de recherche d'un nombre ");
-            int j, min, max;
-            min = 0;
-            max = 0;
-            int[] tableau = new int[4] { 1, 2, 3, 99 };
+            int j;
+            int[] tableau = new int[4] { 99, 24,104,100};
+            int min = tableau[0];
+            int max = tableau[0];
             for (j = 0; j < tableau.Length; j++)
             {
-                if (tableau[j] > max)
+                if  (tableau[j] < min )
                 {
-                    max = tableau[j];
+                    min=tableau[j];
                 }
-                else if (tableau[j] < min)
+                else if (tableau[j] > max && tableau[j]>min)
                 {
-                    min = tableau[j];
+                    max=tableau[j];
                 }
-               
             }
             Console.WriteLine("La valeur minimale est : " + min + "\nLa valeur maximale est: " + max);
         }
